b16220c2d22f3fa1e5add9993198e2c59d95ab4e
Nishanthi G <nishadharani08@gamil.com>
2026-07-17 00:42:58 +0530

    Fix ALLOWED_HOSTS for Render

--- field_worker/settings.py
+++ field_worker/settings.py
@@ -18,9 +18,7 @@ SECRET_KEY = 'django-insecure-5x=956sy&sp77=y-*valmot$d35)iv_6)%)&66#q*6r-3f=%9f
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "fieldworker-crm-3.onrender.com",
+    "*"
 ]
 
 
